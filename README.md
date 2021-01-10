91f0836f652f8f8ab540a179ee14e5a073a656d5
Damian Osipiuk <osipiukd+git@gmail.com>
2021-01-10 02:03:46 +0100

    chore: fix-auto-deploy

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/5ed13c0d-f7f6-41f0-88c6-945a1bec8a2f/deploy-status)](https://app.netlify.com/sites/browserslist-ui/deploys)
 
-Website: https://browserslist-ui.netlify.app/
+Website: https://browserslist.innocore.pl
 
 Web UI to verify browserslist queries and browsers coverage
--- get-package-versions.sh
+++ get-package-versions.sh
@@ -2,4 +2,5 @@
 
 browserslist=$(cut -d "@" -f2 <<<"$(npm list browserslist --depth=0 | grep browserslist@ | xargs)")
 caniuse=$(cut -d "@" -f2 <<<"$(npm list caniuse-lite --depth=1 | grep caniuse-lite@ | xargs)")
+echo "browserslist: $browserslist, caniuse: $caniuse"
 echo "{\"browserslist\": \"$browserslist\",\"caniuse\": \"$caniuse\"}" > ./src/versions.json
